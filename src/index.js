81019f179d3f53aac98f314b1035c01bb806a910
juan45901 <jgcompraslogistica@gmail.com>
2022-10-25 17:45:08 -0500

    frist

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import dotenv from "dotenv";
 import axios from "axios";
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = "https://videogames-api-tau.vercel.app";
 
 ReactDOM.render(
   <Provider store={store}>
